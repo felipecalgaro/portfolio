f184969c802d6a0f3b079e9658ddd20a2f2b7c5e
Felipe <pipecalgaro@gmail.com>
2024-07-31 22:54:24 -0300

    feat: create dedicated file to repositories details

---
+++ src/assets/repositories-details.ts
@@ -0,0 +1,21 @@
+export const repositoriesMappingSchema = {
+  "polls-client": "polls",
+  "polls-api": "polls",
+  "notesql-api": "notesql",
+  "notesql-client": "notesql",
+};
+
+export const unwantedRepositories = ["felipecalgaro", "portfolio"];
+
+export const repositoriesDescriptionMappingSchema = {
+  "polls-client": "",
+  "polls-api": "",
+  "notesql-client": "",
+  "notesql-api": "",
+  "nextjs-ecommerce": "",
+  "password-generator": "",
+  "github-badge": "",
+  "notifications-service": "",
+  "ignite-lab-react": "",
+  "mybrary-fullstack-app": "",
+};
