f809190d2ff2eee8e5dc632fbb95054a0856d9f0
Felipe <pipecalgaro@gmail.com>
2024-08-01 22:46:33 -0300

    fix: update repositories details structure

--- src/assets/repositories-details.ts
+++ src/assets/repositories-details.ts
@@ -1,21 +1,53 @@
-export const repositoriesMappingSchema = {
-  "polls-client": "polls",
-  "polls-api": "polls",
-  "notesql-api": "notesql",
-  "notesql-client": "notesql",
+export const newRepositoriesNames = ["polls", "notesql"] as const;
+
+type NewRepositoriesType = {
+  [newRepoName in (typeof newRepositoriesNames)[number]]: {
+    name: string;
+    description: string;
+  };
 };
+
+const newRepositories = {
+  polls: {
+    name: "polls",
+    description:
+      "Cookies-based platform to create and vote on polls at real-time using WebSocket",
+  },
+  notesql: {
+    name: "notesql",
+    description: "Notes app that combines SOLID principles to a GraphQL API",
+  },
+} as const satisfies NewRepositoriesType;
+
+type RepositoriesMappingSchemaType = {
+  [newRepoName in (typeof newRepositoriesNames)[number]]: {
+    subrepositories: string[];
+    description: (typeof newRepositories)[newRepoName]["description"];
+  };
+};
+
+export const repositoriesMappingSchema = {
+  [newRepositories.polls.name]: {
+    subrepositories: ["polls-api", "polls-client"],
+    description: newRepositories.polls.description,
+  },
+  [newRepositories.notesql.name]: {
+    subrepositories: ["notesql-api", "notesql-client"],
+    description: newRepositories.notesql.description,
+  },
+} satisfies RepositoriesMappingSchemaType;
 
 export const unwantedRepositories = ["felipecalgaro", "portfolio"];
 
-export const repositoriesDescriptionMappingSchema = {
-  "polls-client": "",
-  "polls-api": "",
-  "notesql-client": "",
-  "notesql-api": "",
-  "nextjs-ecommerce": "",
-  "password-generator": "",
-  "github-badge": "",
-  "notifications-service": "",
-  "ignite-lab-react": "",
-  "mybrary-fullstack-app": "",
-};
+// export const repositoriesDescriptionMappingSchema = {
+//   polls:
+//     "Cookies-based platform to create and vote on polls at real-time using WebSocket",
+//   notesql: "",
+//   "nextjs-ecommerce":
+//     "E-commerce platform that integrates coupon, payment and mailing systems",
+//   "password-generator": "",
+//   "github-badge": "",
+//   "notifications-service": "",
+//   "ignite-lab-react": "",
+//   "mybrary-fullstack-app": "",
+// };
