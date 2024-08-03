3af3dc8ad76d98efde4df0a3cdc1d783845cc5b3
Felipe <pipecalgaro@gmail.com>
2024-08-02 21:06:55 -0300

    feat: update repositories mapping structure

--- src/utils/map-repositories.ts
+++ src/utils/map-repositories.ts
@@ -1,3 +1,8 @@
+import {
+  newRepositoriesNames,
+  repositoriesMappingSchema,
+  unwantedRepositories,
+} from "@/assets/repositories-details";
 import { z } from "zod";
 
 export const repositoriesDataSchema = z.array(
@@ -8,10 +13,11 @@ export const repositoriesDataSchema = z.array(
       login: z.string(),
     }),
     html_url: z.string(),
+    description: z.string().nullable(),
   })
 );
 
-type RepositoriesDataSchema = z.infer<typeof repositoriesDataSchema>;
+type RepositoriesDataSchemaType = z.infer<typeof repositoriesDataSchema>;
 
 type MappedRepositoryWithoutSubrepositories = {
   hasSubrepositories: false;
@@ -23,8 +29,8 @@ type MappedRepositoryWithoutSubrepositories = {
 
 type MappedRepositoryWithSubrepositories = {
   hasSubrepositories: true;
-  name: string;
-  description: string;
+  name: (typeof newRepositoriesNames)[number];
+  description: (typeof repositoriesMappingSchema)[(typeof newRepositoriesNames)[number]]["description"];
   subrepositories: {
     id: number;
     name: string;
@@ -37,95 +43,55 @@ type MappedRepositories =
   | MappedRepositoryWithoutSubrepositories;
 
 export class MapRepositories {
-  private newRepositoriesWithDescription: MappedRepositoryWithSubrepositories[] =
-    [];
-  private filteredRepositories: RepositoriesDataSchema = [];
-  private filteredRepositoriesWithDescription: MappedRepositoryWithoutSubrepositories[] =
-    [];
+  private subrepositoriesNames: string[] = [];
 
   constructor(
-    private readonly previousRepositories: RepositoriesDataSchema,
-    private readonly repositoriesMappingSchema: Record<string, string>,
-    private readonly unwantedRepositories: string[],
-    private readonly repositoriesDescriptionMappingSchema: Record<
-      string,
-      string
-    >
+    private readonly previousRepositories: RepositoriesDataSchemaType
   ) {}
 
-  private filter(): void {
-    this.filteredRepositories = Array.from(
-      this.previousRepositories
-        .filter(
-          (repo) =>
-            !this.unwantedRepositories.includes(repo.name) &&
-            repo.owner.login === "felipecalgaro"
-        )
-        .filter((repo) => !(repo.name in this.repositoriesMappingSchema))
-    );
-  }
-
-  private setDescription(
-    newRepositoriesWithoutDescription: Omit<
-      MappedRepositoryWithSubrepositories,
-      "description"
-    >[]
-  ): void {
-    this.newRepositoriesWithDescription = Array.from(
-      newRepositoriesWithoutDescription.map((repo) => ({
+  private filter(): MappedRepositoryWithoutSubrepositories[] {
+    return this.previousRepositories
+      .filter(
+        (repo) =>
+          !unwantedRepositories.includes(repo.name) &&
+          repo.owner.login === "felipecalgaro"
+      )
+      .filter((repo) => !this.subrepositoriesNames.includes(repo.name))
+      .map((repo) => ({
         ...repo,
-        description: this.repositoriesDescriptionMappingSchema[repo.name],
-      }))
-    );
-
-    this.filteredRepositoriesWithDescription = Array.from(
-      this.filteredRepositories.map((repo) => ({
-        ...repo,
-        description: this.repositoriesDescriptionMappingSchema[repo.name],
         hasSubrepositories: false,
-      }))
-    );
+        description: repo.description!,
+      }));
   }
 
   public map(): MappedRepositories[] {
-    this.filter();
+    const newRepositories = newRepositoriesNames.map((newRepoName) => ({
+      name: newRepoName,
+      description: repositoriesMappingSchema[newRepoName].description,
+      hasSubrepositories: true,
+      subrepositories: repositoriesMappingSchema[
+        newRepoName
+      ].subrepositories.map((subrepo) => {
+        this.subrepositoriesNames.push(subrepo);
 
-    const newRepositoriesNames = Array.from(
-      new Set(Object.values(this.repositoriesMappingSchema))
-    );
+        const previousRepo = this.previousRepositories.find(
+          (repo) => repo.name === subrepo
+        );
 
-    const newRepositoriesWithoutDescription = newRepositoriesNames.map(
-      (newRepoName) => ({
-        hasSubrepositories: true,
-        name: newRepoName,
-        subrepositories: Object.keys(this.repositoriesMappingSchema)
-          .filter(
-            (repoName) =>
-              this.repositoriesMappingSchema[
-                repoName as keyof typeof this.repositoriesMappingSchema
-              ] === newRepoName
-          )
-          .map((repoName) => {
-            const repository = this.previousRepositories.find(
-              (repo) => repo.name === repoName
-            );
+        if (!previousRepo) {
+          throw new Error("Unexpected error.");
+        }
 
-            if (!repository) throw new Error("Unexpected error.");
+        return {
+          id: previousRepo.id,
+          name: previousRepo.name,
+          html_url: previousRepo.html_url,
+        };
+      }),
+    })) satisfies MappedRepositoryWithSubrepositories[];
 
-            return {
-              name: repoName,
-              id: repository.id,
-              html_url: repository.html_url,
-            };
-          }),
-      })
-    ) satisfies Omit<MappedRepositoryWithSubrepositories, "description">[];
+    const filteredRepositories = this.filter();
 
-    this.setDescription(newRepositoriesWithoutDescription);
-
-    return [
-      ...this.filteredRepositoriesWithDescription,
-      ...this.newRepositoriesWithDescription,
-    ];
+    return [...filteredRepositories, ...newRepositories];
   }
 }
