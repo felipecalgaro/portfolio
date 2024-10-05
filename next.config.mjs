48cb5d834ca2973e4afa8c519920d06e58d2afcb
Felipe <pipecalgaro@gmail.com>
2024-10-05 16:32:57 -0300

    feat: add url pattern for github avatar

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        pathname: "/felipecalgaro.png",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
