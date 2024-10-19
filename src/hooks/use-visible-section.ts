3e25665ce108dc28f1f4c171ac4bf96f30f2856e
Felipe <pipecalgaro@gmail.com>
2024-10-19 19:30:50 -0300

    feat: create custom hook

---
+++ src/hooks/use-visible-section.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+export function useVisibleSection(sections: string[]) {
+  const [visibleSectionId, setVisibleSectionId] = useState<
+    string | undefined
+  >();
+
+  function isSectionVisible(elementId: string) {
+    const section = document.getElementById(elementId);
+
+    if (section) {
+      const sectionPosition = section.getBoundingClientRect();
+
+      const viewPortHeight = window.innerHeight;
+
+      return sectionPosition.top <= viewPortHeight;
+    }
+
+    return false;
+  }
+
+  useEffect(() => {
+    // perfomance?
+    function checkVisibility() {
+      if (sections.every((id) => !isSectionVisible(id))) {
+        setVisibleSectionId(undefined);
+      }
+      sections.forEach((id) => {
+        if (isSectionVisible(id)) {
+          setVisibleSectionId(id);
+        }
+      });
+    }
+
+    if (sections) {
+      window.addEventListener("scroll", checkVisibility);
+    }
+
+    checkVisibility();
+
+    return () => window.removeEventListener("scroll", checkVisibility);
+  }, [sections]);
+
+  return visibleSectionId;
+}
