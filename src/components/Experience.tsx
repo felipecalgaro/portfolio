3d9060880708f0765dab671943625bd4b1ab6140
Felipe <pipecalgaro@gmail.com>
2024-10-23 14:37:05 -0300

    feat: create Experience component

---
+++ src/components/Experience.tsx
@@ -0,0 +1,13 @@
+import { events } from '@/resources/my-experience';
+import { Event } from './Event';
+
+export function Experience() {
+  return (
+    <div className='flex justify-center items-center flex-col w-full gap-24' id="experience">
+      <h1 className='text-center xs:text-6xl text-5xl font-semibold text-stone-800'>My Experience</h1>
+      <div className='events-arrow-image w-full flex flex-col justify-center items-center gap-20 py-14'>
+        {events.map((event, index) => <Event event={event} key={event.id} index={index} />)}
+      </div>
+    </div>
+  )
+}
