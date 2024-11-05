536ed0f7b01a0c5c0052e146dbee8720104ccd89
Felipe <pipecalgaro@gmail.com>
2024-11-05 20:16:36 -0300

    feat: create Experience component

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -6,7 +6,7 @@ export function Experience() {
     <div className='flex justify-center items-center flex-col w-full gap-24' id="experience">
       <h1 className='text-center xs:text-6xl text-5xl font-semibold text-stone-800'>My Experience</h1>
       <div className='events-arrow-image w-full flex flex-col justify-center items-center gap-20 py-14'>
-        {events.map((event, index) => <Event event={event} key={event.id} index={index} />)}
+        {events.map((event, index) => <Event event={event} key={event.id} isRightOriented={index % 2 === 1} />)}
       </div>
     </div>
   )
