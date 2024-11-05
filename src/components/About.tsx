9cec2eca295b6e5930907e1b01c68b7f3a852ab6
Felipe <pipecalgaro@gmail.com>
2024-11-04 22:07:49 -0300

    fix: add paragraph structure

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,14 @@
-import { aboutMeText } from '@/resources/about-me';
+import { aboutMeParagraphs } from '@/resources/about-me';
 
 export function About() {
   return (
     <div id='about' className='text-stone-800 flex flex-col justify-center items-center gap-24'>
       <h1 className='text-center xs:text-6xl text-5xl font-semibold'>About Me</h1>
-      <p className='max-w-[800px]'>{aboutMeText}</p>
+      <div className='flex justify-center items-center flex-col gap-10 indent-4 max-w-[800px]'>
+        {aboutMeParagraphs.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </div>
     </div>
   )
 }
