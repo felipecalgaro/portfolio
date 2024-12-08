940816b730c89835d2a6d32addb1a54555c9573d
Felipe <pipecalgaro@gmail.com>
2024-12-08 11:11:48 -0300

    fix: add about me text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,11 @@
-import { aboutMeParagraphs } from '@/resources/about-me';
-
 export function About() {
   return (
     <div id='about' className='text-stone-800 flex flex-col justify-center items-center gap-24'>
       <h1 className='text-center xs:text-6xl text-5xl font-semibold'>About Me</h1>
-      <div className='flex justify-center items-center flex-col gap-10 indent-4 max-w-[800px]'>
-        {aboutMeParagraphs.map((paragraph, index) => (
-          <p key={index}>{paragraph}</p>
-        ))}
+      <div className='flex justify-center items-center flex-col gap-6 indent-4 max-w-[800px]'>
+        <p>My name is Felipe and I am a programming enthusiast since 2021, when I watched a free course on the internet which covered the basics of HTML, CSS and Javascript. Immediately fell in love with web development. Nowadays, I enjoy building personal projects from scratch to the point where they are ready to be deployed, be they a website, an API or both. More than anything, I have always given (and will continue to give) preference to challenges that enable the growth of intellectual autonomy, which is proving to be increasingly important nowadays.</p>
+        <p>I am currently studying how the tools I usually use in my projects work under the hood; for instance, how a Node.js framework is built and how to create my own. In fact, just as the physicist Richard Feynman once said - and what I believe to be true when learning anything -, <i>&quot;What I cannot create, I do not understand&quot;</i>. </p>
+        <p>Besides that, I consider myself to be a very self-taught, hard-working person, with great dreams and a will to be an excellent professional. Thinking outside the box is what guides me while programming. Outside of coding, I enjoy going to the gym, reading and learning about economics, which often inspires my work.</p>
       </div>
     </div>
   )
