d87b0c4678339ae2eebdd5e5673deb0ebc959810
Felipe <pipecalgaro@gmail.com>
2025-10-09 17:41:45 +0200

    fix: small text changes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ export function About() {
     <div id='about' className='text-stone-800 flex flex-col justify-center items-center gap-24'>
       <h1 className='text-center xs:text-6xl text-5xl font-semibold'>About Me</h1>
       <div className='flex justify-center items-center flex-col gap-6 indent-4 max-w-[800px]'>
-        <p>My name is Felipe and I am a programming enthusiast since 2021, when I watched a free course on the internet which covered the basics of HTML, CSS and Javascript. Immediately fell in love with web development. The idea of being able to build anything and seeing it come to life on the web is incredibly exciting to me, and is what drives my passion for this field.</p>
+        <p>My name is Felipe and I am a programmer since 2021, when I watched a free course on the internet which covered the basics of HTML, CSS and Javascript. Immediately fell in love with web development. The idea of being able to build anything and seeing it come to life on the web is incredibly exciting to me, and is what drives my passion for this field.</p>
         <p>I am currently aiming at developing apps that solve real-life problems. This makes me study various web development topics more deeply, such as security, authentication and authorization, AI, payment processing, UI/UX, rendering strategies, and even marketing, analytics and entrepreneurship.</p>
         <p>Besides that, I consider myself to be a self-taught, hard-working person, with great dreams and a will to be an excellent professional. Thinking outside the box is what guides me while programming. Outside of coding, I enjoy going to the gym, reading and learning about business, which often inspires my work.</p>
       </div>
